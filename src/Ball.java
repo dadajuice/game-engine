b12c8c40d94d5aeac4ddd3e63eabb63c14903378
David Tucker <davidt2003@msn.com>
2022-09-27 09:03:07 -0400

    refactor: extract class Ball

---
+++ src/Ball.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+import java.util.Random;
+
+public class Ball {
+
+    private int radius;
+    private int x;
+    private int y;
+    private int velocityX;
+    private int velocityY;
+
+    public Ball(int radius) {
+        this.radius = radius;
+        x = randomNumber(radius * 2, 800 - radius * 2);
+        y = randomNumber(radius * 2, 600 - radius * 2);
+        velocityX = randomNumber(0, 1) == 0 ? 5 : -5;
+        velocityY = randomNumber(0, 1) == 0 ? 5 : -5;
+    }
+
+    public void update() {
+        x += velocityX;
+        y += velocityY;
+        if (hasTouchVerticalBound()) {
+            velocityY *= -1;
+        }
+        if (hasTouchHorizontalBound()) {
+            velocityX *= -1;
+        }
+    }
+
+    public boolean hasTouchBound() {
+        return hasTouchHorizontalBound()
+                || hasTouchVerticalBound();
+    }
+
+    private boolean hasTouchVerticalBound() {
+        return y <= radius || y >= 600 - radius;
+    }
+
+    private boolean hasTouchHorizontalBound() {
+        return x <= radius || x >= 800 - radius;
+    }
+
+    public void draw(Graphics2D buffer) {
+        buffer.setPaint(Color.RED);
+        buffer.fillOval(x, y, radius * 2, radius * 2);
+    }
+
+    private int randomNumber(int min, int max) {
+        return (new Random()).nextInt((max - min) + 1) + min;
+    }
+}
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,21 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.Random;
 
 public class GameWindow extends JFrame {
 
     private static final int SLEEP = 25;
 
-    private Random rnd = new Random();
+    private Ball ball;
     private JPanel panel;
     private boolean playing = true;
-    private int radius = 25;
-    private int x = randomNumber(radius * 2, 800 - radius * 2);
-    private int y = randomNumber(radius * 2, 600 - radius * 2);
-    // Vitesse + direction (vélocité)
-    private int dx = randomNumber(0, 1) == 0 ? 5 : -5;
-    private int dy = randomNumber(0, 1) == 0 ? 5 : -5;
     private BufferedImage bufferedImage;
     private Graphics2D buffer;
     private long before;
@@ -35,6 +28,8 @@ public class GameWindow extends JFrame {
         panel.setFocusable(true);
         panel.setDoubleBuffered(true);
         add(panel);
+
+        ball = new Ball(25);
     }
 
     public void start() {
@@ -70,22 +65,14 @@ public class GameWindow extends JFrame {
     }
 
     private void update() {
-        x += dx;
-        y += dy;
-        if (y <= radius || y >= 600 - radius) {
-            dy *= -1;
-            score += 10;
-        }
-        if (x <= radius || x >= 800 - radius) {
-            dx *= -1;
+        ball.update();
+        if (ball.hasTouchBound()) {
             score += 10;
         }
     }
 
     private void drawOnBuffer() {
-        buffer.setPaint(Color.RED);
-        buffer.fillOval(x, y, radius * 2, radius * 2);
-
+        ball.draw(buffer);
         buffer.setPaint(Color.WHITE);
         buffer.drawString("Score: " + score, 10, 20);
     }
@@ -95,9 +82,5 @@ public class GameWindow extends JFrame {
         graphics.drawImage(bufferedImage, 0, 0, panel);
         Toolkit.getDefaultToolkit().sync();
         graphics.dispose();
-    }
-
-    private int randomNumber(int min, int max) {
-        return rnd.nextInt((max - min) + 1) + min;
     }
 }
