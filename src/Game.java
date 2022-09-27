b21b02da9ffa72d1b130e8bfb5a6f2e38b7a8ec0
David Tucker <davidt2003@msn.com>
2022-09-27 09:12:22 -0400

    refactor: inject JFrame object in Game

--- src/Game.java
+++ src/Game.java
@@ -2,11 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class GameWindow extends JFrame {
+public class Game {
 
     private static final int SLEEP = 25;
 
     private Ball ball;
+    private JFrame frame;
     private JPanel panel;
     private boolean playing = true;
     private BufferedImage bufferedImage;
@@ -14,26 +15,14 @@ public class GameWindow extends JFrame {
     private long before;
     private int score = 0;
 
-    public GameWindow() {
-        setSize(800, 600);
-        setLocationRelativeTo(null);
-        setResizable(false);
-        setTitle("My Bouncing Ball");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setState(JFrame.NORMAL);
-        //setUndecorated(true);
-
-        panel = new JPanel();
-        panel.setBackground(Color.BLUE);
-        panel.setFocusable(true);
-        panel.setDoubleBuffered(true);
-        add(panel);
-
+    public Game() {
+        initializeFrame();
+        initializePanel();
         ball = new Ball(25);
     }
 
     public void start() {
-        setVisible(true);
+        frame.setVisible(true);
         before = System.currentTimeMillis();
 
         while (playing) {
@@ -83,4 +72,25 @@ public class GameWindow extends JFrame {
         Toolkit.getDefaultToolkit().sync();
         graphics.dispose();
     }
+
+    private void initializeFrame() {
+        frame = new JFrame();
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setTitle("My Bouncing Ball");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setState(JFrame.NORMAL);
+        //frame.setUndecorated(true);
+    }
+
+    private void initializePanel() {
+        panel = new JPanel();
+        panel.setBackground(Color.BLUE);
+        panel.setFocusable(true);
+        panel.setDoubleBuffered(true);
+        frame.add(panel);
+    }
+
+
 }
